a4fc22b4c284c963bba6e2e8f96fff02e0365b1f
vdi-terminal <vdi-terminal@EDU-WSB.local>
2026-06-07 09:59:16 +0200

    Initial commit on main

---
+++ src/cli.py
@@ -0,0 +1,7 @@
+from app.logic import add
+
+def main():
+    print(add(2, 3))
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_logic.py
@@ -0,0 +1,4 @@
+from app.logic import add
+
+def test_add():
+    assert add(2, 3) == 5
